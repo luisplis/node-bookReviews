3b1f394e769af67029ac37a75eb1a1922a4f2492
luisplis <luisplis@gmail.com>
2026-01-09 15:52:49 +0100

    rutas de usuario generales

--- final_project/index.js
+++ final_project/index.js
@@ -6,17 +6,31 @@ const genl_routes = require('./router/general.js').general;
 
 const app = express();
 
+const SECRET_KEY = "api-books";
+
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer",session({secret: SECRET_KEY,resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+  // OK
+  const token = req.session.authorization;
+  if (token) {
+      jwt.verify(token, SECRET_KEY, (err, user) => {
+      if (err) {
+        return res.status(403).json({message: "User not authenticated"});
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(403).json({message: "User not authenticated"});
+  }
 });
  
-const PORT =5000;
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, ()=>console.log("Server is running!"));
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,40 +4,69 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const user = req.body.username;
+  const pass = req.body.password;
+  
+  if (!user || !pass) {
+      return res.status(404).json({ message: "Error: username or password inexistent" });
+  }
+  
+  let exists = users.filter((user) => {
+    return (user.username === user && user.password === pass);
+  });
+  
+  if (exists.length)
+    return res.status(404).json({message: "User already exists!"});
+
+  users.push({"username": user, "password": pass});
+
+  return res.json({message: `User '${user}' registered successfully`});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books, null, 2)); // OK
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const book = books[req.params.isbn]; 
+  if (book)
+    return res.send(JSON.stringify(book, null, 2));
+  
+  return res.status(404).json({message: "Book not found"});
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const book = Object.values(books).filter((book) => {
+    return book.author.toLocaleLowerCase().includes(req.params.author.toLocaleLowerCase());
+  });
+
+  if (book.length) return res.send(JSON.stringify(book, null, 2));
+  
+  return res.status(404).json({message: "Books not found"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const book = Object.values(books).filter((book) => {
+    return book.title.toLocaleLowerCase().includes(req.params.title.toLocaleLowerCase());
+  });
+  
+  if (book.length) return res.send(JSON.stringify(book, null, 2));
+  
+  return res.status(404).json({message: "Books not found"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const reviews = books[req.params.isbn].reviews;
+  
+  if (reviews) return res.send(JSON.stringify(reviews, null, 2));
+  
+  return res.status(404).json({message: "Empty reviews for this book"});
 });
 
 module.exports.general = public_users;
