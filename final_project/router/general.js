05cd99894049d181eb14c31ee5439358f15c3d5a
luisplis <luisplis@gmail.com>
2026-01-12 17:55:46 +0100

    Tarea 13 terminada

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,6 +2,7 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
+
 const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
@@ -26,7 +27,32 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.send(JSON.stringify(books, null, 2)); // OK
+  return res.send(JSON.stringify(books, null, 2)); // OK - Task 1-4
+});
+
+// ASYNC/AWAIT
+public_users.get('/books',function (req, res) {
+
+  (async () => {
+      try {
+        const response = await fetch('http://localhost:5000/');
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        
+        return res.send(JSON.stringify(data, null, 2));
+      } 
+      catch (error) {
+        
+        console.error("Fetch Error on GET books:", error.message);
+
+        return res.status(500).json({message: "Error fetching data"});
+      }
+  })();
+
 });
 
 // Get book details based on ISBN
@@ -37,7 +63,30 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
   return res.status(404).json({message: "Book not found"});
 });
-  
+
+// ASYNC/AWAIT ISBN
+public_users.get('/books/isbn/:isbn',function (req, res) {
+ 
+  const isbn  = req.params.isbn;
+  (async () => {
+    try {
+      const response = await fetch('http://localhost:5000/isbn/'+isbn);
+
+      if (!response.ok) {
+          return res.status(response.status).send("Error fetching by isbn");
+      }
+      const data = await response.json();
+
+      return res.send(JSON.stringify(data, null, 2));
+    } 
+    catch (error) {
+      console.error("Fetch Error on GET books:", error.message);
+      return res.status(500).send("Internal Server Error");
+    }
+  })();
+
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   const book = Object.values(books).filter((book) => {
@@ -49,6 +98,29 @@ public_users.get('/author/:author',function (req, res) {
   return res.status(404).json({message: "Books not found"});
 });
 
+// ASYNC/AWAIT AUTHOR
+public_users.get('/books/author/:author',function (req, res) {
+ 
+  const author  = req.params.author;
+  (async () => {
+    try {
+      const response = await fetch('http://localhost:5000/author/'+author);
+
+      if (!response.ok) {
+          return res.status(response.status).send("Error fetching book by author");
+      }
+      const data = await response.json();
+
+      return res.send(JSON.stringify(data, null, 2));
+    } 
+    catch (error) {
+      console.error("Fetch Error on GET books:", error.message);
+      return res.status(500).send("Internal Server Error");
+    }
+  })();
+
+});
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   const book = Object.values(books).filter((book) => {
@@ -58,6 +130,30 @@ public_users.get('/title/:title',function (req, res) {
   if (book.length) return res.send(JSON.stringify(book, null, 2));
   
   return res.status(404).json({message: "Books not found"});
+});
+
+
+// ASYNC/AWAIT ISBN
+public_users.get('/books/title/:title',function (req, res) {
+ 
+  const title  = req.params.title;
+  (async () => {
+    try {
+      const response = await fetch('http://localhost:5000/title/'+title);
+
+      if (!response.ok) {
+          return res.status(response.status).send("Error fetching book by title");
+      }
+      const data = await response.json();
+
+      return res.send(JSON.stringify(data, null, 2));
+    } 
+    catch (error) {
+      console.error("Fetch Error on GET books:", error.message);
+      return res.status(500).send("Internal Server Error");
+    }
+  })();
+
 });
 
 //  Get book review
